3aeb95076e39ea94c8fb0ffaad1d9c79508437a6
Eduardo Nunes <eduardo.nunes@4gautomacao.com.br>
2023-01-18 15:56:17 -0300

    Projeto Buscante

--- src/app/views/lista-livros/lista-livros.component.ts
+++ src/app/views/lista-livros/lista-livros.component.ts
@@ -1,5 +1,12 @@
+import { LivroVolumeInfo } from './../../models/livroVolumeInfo';
+import { Item, LivrosResultado } from './../../models/interfaces';
+import { LivroService } from './../../service/livro.service';
 import { Component } from '@angular/core';
+import { catchError, debounceTime, distinctUntilChanged, filter, map, of, switchMap, tap, throwError } from 'rxjs';
+import { FormControl } from '@angular/forms';
 
+
+const PAUSA = 300
 @Component({
   selector: 'app-lista-livros',
   templateUrl: './lista-livros.component.html',
@@ -7,11 +14,47 @@ import { Component } from '@angular/core';
 })
 export class ListaLivrosComponent {
 
-  listaLivros: [];
+  campoBusca = new FormControl()
+  mensagemErro = ''
+  semResultados = ''
+  livrosResultado: number
 
-  constructor() { }
+  constructor(private service: LivroService) { }
+
+
+  livrosEncontrados$ = this.campoBusca.valueChanges
+    .pipe(
+      debounceTime(PAUSA),
+      filter((valorDigitado) => valorDigitado.length >= 3),
+      distinctUntilChanged(),
+      tap(() => console.log('Fluxo inicial')),
+      switchMap(valorDigitado =>
+        this.service.buscar(valorDigitado)
+      ),
+      map(resultado => {
+        this.livrosResultado = resultado.totalItems
+        if (!resultado.items) {
+          this.semResultados = 'Não foi encontrado nenhum livro com os termos buscados.'
+        }
+        return resultado.items ?? []
+
+      }),
+      map(items => this.resultadoLivros(items)),
+      catchError(error => {
+        console.log(error)
+        return throwError(() => new Error(this.mensagemErro = 'Ops! Houve um problema... Recarregue a página.'))
+      })
+    )
+
+  resultadoLivros(items: Item[]): LivroVolumeInfo[] {
+    return items.map(item => {
+      return new LivroVolumeInfo(item)
+    })
+  }
 
 }
+
+
 
 
 
